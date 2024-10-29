428eafaaa59f57789886ff3d01190afb731e4545
Miguel <migueloliveira68910@gmail.com>
2024-10-29 14:58:13 -0300

    criei uma branch chamada  pag-cadastro-login, criei uma pasta  chamada controller e dentro dela adicionei uma classe para se conectar com o sql. Alterei o btncadastro para outra forma de cadastrar o usuario

---
+++ Controller/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using agenda_project.Data;
+
+namespace agenda_project.Controller
+{
+    internal class UsuarioController
+    {
+        public bool AddUsuario(string nome, string usuario, string telefone, string senha)
+        {
+            
+            MySqlConnection conexao = Conexao_database.CriaConexao();
+
+            string sql = "INSERT INTO tbUsuarios (nome, usuario, telefone, senha) values  (@nome, @usuario, @telefone, @senha);";
+
+            conexao.Open();
+
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            comando.Parameters.AddWithValue("@nome", nome);
+
+            comando.Parameters.AddWithValue("@usuario", usuario);
+
+            comando.Parameters.AddWithValue("@telefone", telefone);
+
+            comando.Parameters.AddWithValue("@senha", senha);
+
+            //executando comando no banco de dados
+            int LinhasAfetadas = comando.ExecuteNonQuery();
+
+            conexao.Close();
+
+            if (LinhasAfetadas > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+}
+    }
+
---
+++ EstruturaDB/agendaDB.sql
@@ -0,0 +1,7 @@
+create database dbAgenda;
+create table tb_usuario
+(nome varchar(50) not null,
+ usuario varchar (30) primary key, 
+ telefone varchar(15) not null,
+ senha varchar(30) not null
+);
